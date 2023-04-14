a4400fb61e5384338d03017347d717f4c0b8f9f7
Thiago Neves <thiago@weberous.com>
2023-04-14 19:12:43 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import Alpine from 'alpinejs'
+import message from "./message";
+import './css/style.css';
+
+// IN BETWEEN alpine code goes here
+
+window.Alpine = Alpine 
+Alpine.start()
+
+console.log(message);
